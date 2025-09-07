773092dd8c78b14ad2d54beaed1b2280f825c114
nazario88 <nazario.addict@gmail.com>
2025-09-07 20:42:44 +0200

    suite

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
-// config.example.php
+// config.php
 return [
-    'STRAVA_CLIENT_ID' => 'votre_client_id',
-    'STRAVA_CLIENT_SECRET' => 'votre_client_secret'
+    'STRAVA_CLIENT_ID' => '152886',
+    'STRAVA_CLIENT_SECRET' => '77d59190f26e2271da166cb2771d6720007e6bf6'
 ];
